89a4fb59e3e04659ceeb09c46a8de4a35bb02b72
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2021-03-16 10:25:30 +0100

    Initial Commit

---
+++ app/src/main/java/com/htlgkr/pos3/feinboeck18/noteapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.htlgkr.pos3.feinboeck18.noteapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
